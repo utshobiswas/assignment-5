9c2d86cebac56c2d95db2e3a0904b6d911e64d47
utshobiswas <utshobsws@gmail.com>
2024-09-24 23:45:48 +0600

    added notification history and modal

--- js/index.js
+++ js/index.js
@@ -8,16 +8,18 @@ for (let singleDonationSection of donationSections) {
     const donateBtn = singleDonationSection.querySelector(".donate-btn");
     const donationAmountSpan = singleDonationSection.querySelector("span");
 
+    const donationField = singleDonationSection.querySelector(".donate-text");
+
     const inputField = singleDonationSection.querySelector('input');
 
     donateBtn.addEventListener('click', function () {
         // updating the section donation
         const amount = Number(inputField.value);
         
-        console.log(amount, balanceAmount);
         if (isNaN(amount) || amount <= 0 || amount > parseFloat(balanceAmount.innerText)) {
             inputField.value = null;
             alert('Invalid Donation Amount');
+            document.getElementById("my_modal_5").close()
             return;
         }
         // if (isNaN(amount) || amount == 0) {
@@ -33,13 +35,14 @@ for (let singleDonationSection of donationSections) {
         const updateBalance = currentBalance - amount;
         balanceAmount.innerText = updateBalance.toFixed(2);
         inputField.value = null;
+        document.getElementById("my_modal_5").showModal();
 
         // history tab items
         const historyItem = document.createElement("div");
         historyItem.className = "border-2 border-secondary rounded-2xl p-8";
 
         historyItem.innerHTML = `
-    <p class="text-xl font-bold"> ${amount.toFixed(2)} Taka is $</p>
+    <p class="text-xl font-bold"> ${amount.toFixed(2)} Taka is Donated for ${donationField.innerText} Bangladesh</p>
     <p class="font-light">Date: ${new Date().toLocaleString()}</p>
         
   
